711632cf74360fe9f3f758821bd154b6dadf8c9f
Mateo Alvarez <cristian.alvarezp@uniagustiniana.edu.co>
2022-09-04 23:35:52 -0500

    Primer commit, creo el repositorio

---
+++ dreams.php
@@ -0,0 +1,50 @@
+<?php include ("template/cabecera.php")?>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/RedPandaaa.webp" alt="">
+
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/aBadAssRedPanda.webp" alt="">
+
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/aREdPanda.png" alt="">
+
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/cuteRedPanda.webp" alt="">
+
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include("template/pie.php")?>
