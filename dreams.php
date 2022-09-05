aeefef5b20ad8c82801f52ac074a09ee25531c64
Mateo Alvarez <yaalsims@gmail.com>
2022-09-05 00:44:13 -0500

    Actualizacion de titulos de tarjetas

--- dreams.php
+++ dreams.php
@@ -5,7 +5,7 @@
         <img class="card-img-top" src="./img/RedPandaaa.webp" alt="">
 
         <div class="card-body">
-            <h4 class="card-title">Title</h4>
+            <h4 class="card-title">Panda rojo de guerra</h4>
             <p class="card-text">Text</p>
         </div>
     </div>
@@ -16,7 +16,7 @@
         <img class="card-img-top" src="./img/aBadAssRedPanda.webp" alt="">
 
         <div class="card-body">
-            <h4 class="card-title">Title</h4>
+            <h4 class="card-title">Panda rojo astuto</h4>
             <p class="card-text">Text</p>
         </div>
     </div>
@@ -28,7 +28,7 @@
         <img class="card-img-top" src="./img/aREdPanda.png" alt="">
 
         <div class="card-body">
-            <h4 class="card-title">Title</h4>
+            <h4 class="card-title">Panda rojo juega videojuegos</h4>
             <p class="card-text">Text</p>
         </div>
     </div>
@@ -39,7 +39,7 @@
         <img class="card-img-top" src="./img/cuteRedPanda.webp" alt="">
 
         <div class="card-body">
-            <h4 class="card-title">Title</h4>
+            <h4 class="card-title">Panda rojo tierno</h4>
             <p class="card-text">Text</p>
         </div>
     </div>
